6a6d76224e82bb0f97ce2696ca85b42355797572
huyenpham99 <53186394+huyenpham99@users.noreply.github.com>
2020-06-15 11:04:55 +0700

    push

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -33,9 +33,9 @@ class HomeController extends Controller
 //            $p->save();
 //        }
 //        die("done");
-        $u = Auth::user();
-        $u->role =User::ADMIN_ROLE;
-        $u->save();
+//        $u = Auth::user();
+//        $u->role =User::ADMIN_ROLE;
+//        $u->save();
         if (!Cache::has("home_page")){
             $most_views = Product::orderBy("view_count", "DESC")->limit(8)->get();
             $featured = Product::orderBy("updated_at", "DESC")->limit(8)->get();
