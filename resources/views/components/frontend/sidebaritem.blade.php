8c195173afdee5c836155162318911da7a63a65d
huyenpham99 <53186394+huyenpham99@users.noreply.github.com>
2020-06-16 15:06:39 +0700

    push

--- resources/views/components/frontend/sidebaritem.blade.php
+++ resources/views/components/frontend/sidebaritem.blade.php
@@ -1,6 +1,7 @@
 <div class="sidebar__item">
-    <h4>Department</h4>
-    @foreach(\App\Category::all() as $category)
-       <li><a href="{{$category->getCategoryUrl()}}">{{$category->__get("category_name")}}</a></li>
-    @endforeach
+    <ul>
+        @foreach(\App\Category::all() as $category)
+            <li><a href="{{$category->getCategoryUrl()}}">{{$category->__get("category_name")}}</a></li>
+        @endforeach
+    </ul>
 </div>
