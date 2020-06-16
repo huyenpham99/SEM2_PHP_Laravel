b671beb4001c0b7565a1035c79f1b4e7ba85d2a9
huyenpham99 <53186394+huyenpham99@users.noreply.github.com>
2020-06-16 15:55:57 +0700

    fixbug

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -27,16 +27,16 @@ class HomeController extends Controller
     public function index()
     {
         $products = Product::all();
-        foreach ($products as $p){
+        foreach ($products as $p) {
             $slug = \Illuminate\Support\Str::slug($p->__get("product_name"));
-            $p->slug =$slug.$p->__get("id");
+            $p->slug = $slug . $p->__get("id");
             $p->save();
         }
 //        die("done");
 //        $u = Auth::user();
 //        $u->role =User::ADMIN_ROLE;
 //        $u->save();
-        if (!Cache::has("home_page")){
+        if (!Cache::has("home_page")) {
             $most_views = Product::orderBy("view_count", "DESC")->limit(8)->get();
             $featured = Product::orderBy("updated_at", "DESC")->limit(8)->get();
             $latest_1 = Product::orderBy("updated_at", "DESC")->limit(3)->get();
@@ -53,7 +53,7 @@ class HomeController extends Controller
             $now = Carbon::now();
             Cache::put("home_page", $view, $now->addMinutes(20));
         }
-return  Cache::get("home_page");
+        return Cache::get("home_page");
     }
 
     public function category(Category $category)
@@ -84,15 +84,16 @@ return  Cache::get("home_page");
         ]);
     }
 
-    public function addToCart(Product $product,Request $request){
-        $qty = $request->has("qty") && (int)$request->get("qty")>0?(int)$request->get("qty"):1;// kiểm tra qty co phai number hay khong
+    public function addToCart(Product $product, Request $request)
+    {
+        $qty = $request->has("qty") && (int)$request->get("qty") > 0 ? (int)$request->get("qty") : 1;// kiểm tra qty co phai number hay khong
 //        dd($qty);
         // lay qty kiem tra neu la int > 0 thi se tra ve = qty = 1
-        $myCart = session()->has("my_cart") && is_array(session("my_cart"))?session("my_cart"):[];
+        $myCart = session()->has("my_cart") && is_array(session("my_cart")) ? session("my_cart") : [];
 //        dd($myCart);
         // kiem tra session neu co truong my_cart va mang my_cart neu khong co se truyen vao 1 mang rong~
         // nguyen tac lam trang gio hang se tang so luong chu khong tang san pham vao
-        if(Auth::check()) {
+        if (Auth::check()) {
             if (Cart::where("user_id", Auth::id())->where("is_checkout", true)->exists()) {
                 $cart = Cart::where("user_id", Auth::id())->where("is_checkout", true)->first();
             } else {
@@ -104,27 +105,27 @@ return  Cache::get("home_page");
             }
         }
         $contain = false;
-        foreach ($myCart as $key=>$item){ // dua vao key de lay lai doi tuong item
-            if($item["product_id"] == $product->__get("id")){ // nếu sản phẩm đã có trong giỏ
+        foreach ($myCart as $key => $item) { // dua vao key de lay lai doi tuong item
+            if ($item["product_id"] == $product->__get("id")) { // nếu sản phẩm đã có trong giỏ
                 $myCart[$key]["qty"] += $qty; // nếu có thì sẽ truyền thêm vào biến qty ở trên
                 $contain = true; // neu co san pham se truyen trang thai ve true
-                if(Auth::check()){
-                    DB::table("cart_product")->where("cart_id",$cart->__get("id"))
-                        ->where("product_id",$item["product_id"])
+                if (Auth::check()) {
+                    DB::table("cart_product")->where("cart_id", $cart->__get("id"))
+                        ->where("product_id", $item["product_id"])
                         ->update([
-                            "qty"=>$myCart[$key]["qty"]
+                            "qty" => $myCart[$key]["qty"]
                         ]);
                 }
                 break;
             }
         }
         // dat 1 bien de kiem tra trang thai san pham co hay chua
-        if(!$contain){ // nếu trả về true sẽ trả về 1 mảng mycart mới truyền vào qty và id sản phẩm hiện tại
+        if (!$contain) { // nếu trả về true sẽ trả về 1 mảng mycart mới truyền vào qty và id sản phẩm hiện tại
             $myCart[] = [
                 "product_id" => $product->__get("id"),
                 "qty" => $qty
             ];
-            if (Auth::check()){
+            if (Auth::check()) {
                 DB::table("cart_product")->insert([
                     "qty" => $qty,
                     "cart_id" => $cart->__get("id"),
@@ -141,23 +142,25 @@ return  Cache::get("home_page");
         // return redirect về trang trước
         return redirect()->back();
     }
-    public function shoppingCart(){
-        $myCart = session()->has("my_cart") && is_array(session("my_cart"))?session("my_cart"):[];
+
+    public function shoppingCart()
+    {
+        $myCart = session()->has("my_cart") && is_array(session("my_cart")) ? session("my_cart") : [];
         $products = [];
-        foreach ($myCart as $item){
+        foreach ($myCart as $item) {
             $products[] = $item["product_id"];
         }
         $grandTotal = 0;
         $products = \App\Product::find($products);
-        foreach ($products as $p){
-            foreach ($myCart as $item){
-                if($p->__get("id") == $item["product_id"]){
+        foreach ($products as $p) {
+            foreach ($myCart as $item) {
+                if ($p->__get("id") == $item["product_id"]) {
                     $grandTotal += ($p->__get("price") * $item["qty"]);
                     $p->cart_qty = $item["qty"]; // them doi tuong cart_qty de foreach ra mang
                 }
             }
         }
-        return view("frontend.cart",[
+        return view("frontend.cart", [
             "products" => $products,
             "grandTotal" => $grandTotal,
         ]);
@@ -170,46 +173,49 @@ return  Cache::get("home_page");
             ->with("getItems")
             ->firstOrFail();
         return view("frontend.checkout", [
-            "cart" =>$cart
+            "cart" => $cart
         ]);
     }
+
     //xu ly chuc nang thanh toan
 
-public function placeOrder(Request $request){
+    public function placeOrder(Request $request)
+    {
         $request->validate([
-            "username"=>"required",
-            "address"=>"required",
-            "telephone"=>"required"
+            "username" => "required",
+            "address" => "required",
+            "telephone" => "required"
         ]);
-        $cart =Cart::where("user_id", Auth::id())
+        $cart = Cart::where("user_id", Auth::id())
             ->where("is_check", true)
             ->where("getItems")
             ->firstOrFail();
-    $grandTotal=0;
-        foreach ($cart->getItems as $item){
-            $grandTotal += $item->pivot->__get("qty") *$item->__get("price");
+        $grandTotal = 0;
+        foreach ($cart->getItems as $item) {
+            $grandTotal += $item->pivot->__get("qty") * $item->__get("price");
         }
-    try {
-        $order = Order::create([
-            "user_id"=>Auth::id(),
-            "username"=>$request->get("username"),
-            "address"=>$request->get("adress"),
-            "telephone"=>$request->get("telephone"),
-            "note"=>$request->get("note"),
-            "status"=>Order::PENDING
-        ]);
-        foreach ($cart->getItems as $item){
-            DB::table("orders_products")->insert([
-                "order_id"=>$order->__get("id"),
-                "product_id"=>$item->__get("product_id"),
-                "price"=>$item->__get("price"),
-                "qty"=>$item->pivot->__get("qty"),
+        try {
+            $order = Order::create([
+                "user_id" => Auth::id(),
+                "username" => $request->get("username"),
+                "address" => $request->get("address"),
+                "telephone" => $request->get("telephone"),
+                "note" => $request->get("note"),
+                "grand_total" => $grandTotal,
+                "status" => Order::PENDING
             ]);
+            foreach ($cart->getItems as $item) {
+                DB::table("orders_products")->insert([
+                    "order_id" => $order->__get("id"),
+                    "product_id" => $item->__get("product_id"),
+                    "price" => $item->__get("price"),
+                    "qty" => $item->pivot->__get("qty"),
+                ]);
 
+            }
+            event(new OrderCreated($order));
+        } catch (\Exception $exception) {
+            return redirect()->back();
         }
-        event(new OrderCreated($order));
-    }catch (\Exception $exception){
-
     }
-}
 }
